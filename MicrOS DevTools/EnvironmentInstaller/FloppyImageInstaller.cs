9e33f67e30ab2a9a85e63d196da6844ad8cd69c5
tearth <tearthdev@gmail.com>
2019-02-23 17:19:25 +0100

    Add floppy image installer.

---
+++ MicrOS DevTools/EnvironmentInstaller/FloppyImageInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net;
+
+namespace MicrOS_DevTools.EnvironmentInstaller
+{
+    public class FloppyImageInstaller
+    {
+        public void Install(string repositoryPath, string projectPath)
+        {
+            using (var webClient = new WebClient())
+            using (var floppyWriter = new StreamWriter(Path.Combine(projectPath, "Build/floppy.img")))
+            {
+                var floppyData = webClient.DownloadData(Path.Combine(repositoryPath, "install/floppy.img"));
+                floppyWriter.Write(floppyData);
+            }
+        }
+    }
+}
