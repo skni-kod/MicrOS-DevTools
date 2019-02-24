b6e6b4e5561fd15160f0e8dc358dd9c0b1b53f9b
tearth <tearthdev@gmail.com>
2019-02-24 14:35:50 +0100

    Add process terminator.

--- MicrOS DevTools Updater/Forms/MainForm.cs
+++ MicrOS DevTools Updater/Forms/MainForm.cs
@@ -1,16 +1,22 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using MicrOS_DevTools_Updater.Settings;
+using MicrOS_DevTools_Updater.Updater;
 
 namespace MicrOS_DevTools_Updater.Forms
 {
     public partial class MainForm : Form
     {
         private readonly SettingsManager _settingsManager;
+        private readonly VersionChecker _versionChecker;
+        private readonly ProcessTerminator _processTerminator;
         private SettingsContainer _settingsContainer;
 
         public MainForm()
         {
             _settingsManager = new SettingsManager();
+            _versionChecker = new VersionChecker();
+            _processTerminator = new ProcessTerminator();
 
             InitializeComponent();
         }
@@ -18,6 +24,23 @@ namespace MicrOS_DevTools_Updater.Forms
         private async void MainForm_Load(object sender, System.EventArgs e)
         {
             _settingsContainer = await _settingsManager.LoadAsync("settings.json");
+
+            var remoteAppVersion = await _versionChecker.GetRemoteConfigurationVersion(_settingsContainer.RepositoryLink);
+            if (remoteAppVersion != _settingsContainer.AppVersion)
+            {
+                var result = MessageBox.Show(
+                    "Dostępna jest nowa wersja aplikacji MicrOS DevTools. Czy chcesz ją teraz zaktualizować?",
+                    "Aktualizacja", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    _processTerminator.Terminate("MicrOS DevTools");
+                }
+                else if (result == DialogResult.No)
+                {
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
---
+++ MicrOS DevTools Updater/Updater/ProcessTerminator.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace MicrOS_DevTools_Updater.Updater
+{
+    public class ProcessTerminator
+    {
+        public void Terminate(string processName)
+        {
+            var processes = Process.GetProcesses();
+            var targets = processes.Where(p => p.ProcessName == processName);
+
+            foreach(var processToTerminate in targets)
+            {
+                processToTerminate.Kill();
+            }
+        }
+    }
+}
