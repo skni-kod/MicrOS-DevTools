51ed41e5cc54de2be21b5d787119ad824ec2d73e
tearth <tearthdev@gmail.com>
2019-02-24 14:00:35 +0100

    Add updater project.

---
+++ MicrOS DevTools Updater/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MicrOS_DevTools_Updater
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
