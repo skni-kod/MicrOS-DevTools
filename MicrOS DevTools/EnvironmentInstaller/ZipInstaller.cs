2252c8aa7c7a29dfed0e318a8051df83ecd8e2c8
tearth <tearthdev@gmail.com>
2019-02-26 23:52:20 +0100

    Allow ZipInstaller to overwrite files.

--- MicrOS DevTools/EnvironmentInstaller/ZipInstaller.cs
+++ MicrOS DevTools/EnvironmentInstaller/ZipInstaller.cs
@@ -15,7 +15,24 @@ namespace MicrOS_DevTools.EnvironmentInstaller
             using (var webClient = new WebClient())
             {
                 await webClient.DownloadFileTaskAsync(Path.Combine(repositoryPath, zip), Path.Combine(targetDirectory, fileName));
-                await Task.Run(() => ZipFile.ExtractToDirectory(Path.Combine(targetDirectory, fileName), targetDirectory));
+                await Task.Run(() =>
+                {
+                    using (var streamReader = new StreamReader(Path.Combine(targetDirectory, fileName)))
+                    using (var zipArchive = new ZipArchive(streamReader.BaseStream))
+                    {
+                        foreach (var fileToExtract in zipArchive.Entries)
+                        {
+                            var extractedFileName = Path.Combine(targetDirectory, fileToExtract.FullName);
+                            if (fileToExtract.Name == string.Empty)
+                            {
+                                Directory.CreateDirectory(Path.GetDirectoryName(extractedFileName));
+                                continue;
+                            }
+
+                            fileToExtract.ExtractToFile(extractedFileName, true);
+                        }
+                    }
+                });
             }
 
             File.Delete(Path.Combine(targetDirectory, fileName));
